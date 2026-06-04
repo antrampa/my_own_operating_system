8c91bf7c3c0332fda9433d0b1092c03757a0544c
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-06-05 01:33:42 +0300

    print welcome message in red

--- loader.asm
+++ loader.asm
@@ -1,11 +1,24 @@
 [BITS 16]
 [ORG 0x7E00]
 
+
 start:
+    ;Welcome Message
+    mov ah, 0x13
+    mov al, 1
+    mov bx, 0xc
+    ;mov dh, 0
+    ;mov dl, 5
+    mov dx, 0x0000 ; ; row 0, col 5
+    mov bp, WelcomeMessage
+    mov cx, WelcomeMessageLen
+    int 0x10
+
     mov ah, 0x13
     mov al, 1
     mov bx, 0xa
-    xor dx, dx
+    ;xor dx, dx ; row 0, col 0
+    mov dx, 0x0100 ; ; row 1, col 0
     mov bp, Message
     mov cx, MessageLen
     int 0x10
@@ -13,7 +26,9 @@ End:
     hlt
     jmp End
 
-Message:    db "Welcome :: Loader starts"
+WelcomeMessage:     db "!!! Welcome to our Custom OS !!!"
+WelcomeMessageLen:  equ $-WelcomeMessage 
+Message:    db "Loader starts"
 MessageLen: equ $-Message
 
 
